7358ba625577e4952f505fa0368a56e6ed98f40b
hllee <hyelynn1@naver.com>
2024-10-25 18:30:38 +0900

    - 상품페이지 flex-grow 추가

--- app/product/layout.tsx
+++ app/product/layout.tsx
@@ -7,9 +7,9 @@ export default function ProductLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800">
+    <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800">
       <HeroSection />
-      <main className="container mx-auto px-4 py-8">
+      <main className="container mx-auto px-4 py-8 flex-grow">
         {children}
       </main>
       <Footer />
