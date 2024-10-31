6a6959cc09d03203a67110d9b1a1ec6590a52012
hllee <hyelynn1@naver.com>
2024-10-31 23:09:28 +0900

    - z-index 수정

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -31,7 +31,7 @@ export function SearchBar() {
   }, []);
 
   return (
-    <div className={`fixed left-0 right-0 z-50 transition-all duration-300 ease-in-out ${isScrolled ? 'top-4 h-12' : 'top-16 h-20'} bg-white shadow-md`}>
+    <div className={`fixed left-0 right-0 z-50 transition-all duration-300 ease-in-out ${isScrolled ? 'top-4 h-12' : 'top-16 h-20'} bg-white te shadow-md`}>
       <div className={`flex items-center justify-center h-full px-4 ${isScrolled ? 'mt-2' : ''}`}>
         <div className={`relative flex items-center w-full -mt-7 ${isScrolled ? 'max-w-md' : 'max-w-2xl'}`}>
           <Input
@@ -43,7 +43,7 @@ export function SearchBar() {
             className={`pl-4 pr-10 py-2 w-full text-gray-800 rounded-full border-2 ${isScrolled ? 'py-1' : 'py-2 h-14'} transition-all duration-300`}
           />
           <Search
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-primary transition-transform duration-300"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-primary transition-transform duration-300" style={{zIndex:'99'}}
             onClick={handleIconClick}
           />
         </div>
